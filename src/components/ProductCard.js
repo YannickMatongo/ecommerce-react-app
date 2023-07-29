fc214540a6f988410a867e4ed8c5b739fc81a823
Yannick Matongo <nay77127@gmail.com>
2023-07-29 14:50:35 +0200

    first commit

---
+++ src/components/ProductCard.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+import { Link } from "react-router-dom";
+
+function ProductCard() {
+  return (
+    <div className="col-3">
+      <Link className="product-card position-relative">
+        <div className="wishlist-icon position-absolute">
+          <Link>
+            <img src="images/wish.svg" alt="" />
+          </Link>
+        </div>
+        <div className="product-image">
+          <img src="images/smartwatch.jpg" alt="product image" />
+          {/* image transition */}
+          <img src="images/smartwatch-1.jpg" alt="product image" />
+        </div>
+        <div className="product-details">
+          <h6 className="brand">Havels </h6>
+          <h5 className="product-title">
+            Kids headphones bulk pack multi colored for students
+          </h5>
+          <p className="price">€100.00</p>
+          <ReactStars
+            count={5}
+            size={24}
+            value={4}
+            // edit={false}
+            activeColor="#ffd700"
+          />
+        </div>
+        <div className="action-bar position-absolute">
+          <div className="d-flex flex-column gap-15">
+            <Link>
+              <img src="images/prodcompare.svg" alt="productcompare" />
+            </Link>
+          </div>
+          <div className="d-flex flex-column">
+            <Link>
+              <img src="images/view.svg" alt="view" />
+            </Link>
+          </div>
+          <div className="d-flex flex-column">
+            <Link>
+              <img src="images/add-cart.svg" alt="add" />
+            </Link>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default ProductCard;
