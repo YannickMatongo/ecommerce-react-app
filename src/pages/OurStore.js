9ba4af59e0c886fd05b60e32be360332a45d4702
Yannick Matongo <nay77127@gmail.com>
2023-07-30 10:28:17 +0200

    add sorty by

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -2,6 +2,7 @@ import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import { Helmet } from "react-helmet";
 import Meta from "../components/Meta";
+import ReactStars from "react-rating-stars-component";
 
 function OurStore() {
   return (
@@ -118,34 +119,112 @@ function OurStore() {
                   </div>
                 </div>
               </div>
-              {/* Product Tage */}
+              {/* Product Tag */}
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Product Tags</h3>
                 <div>
-                    <div className="product-tags d-flex flex-wrap align-items-lg-center gap-10">
-                        <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
-                            Headphone
-                        </span>
-                        <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
-                            Laptop
-                        </span>
-                        <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
-                            Mobile
-                        </span>
-                        <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
-                            Wire
-                        </span>
-                    </div>
+                  <div className="product-tags d-flex flex-wrap align-items-lg-center gap-10">
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Headphone
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Laptop
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Mobile
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Wire
+                    </span>
+                  </div>
                 </div>
               </div>
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Random Product</h3>
                 <div>
-                    
+                  <div className="random-products mb-3 d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="watch"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>
+                        Kids headphones bulk 10 pack multi colored for students
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        // edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b> 300 €</b>
+                    </div>
+                  </div>
+                  <div className="random-products d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="watch"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>
+                        Kids headphones bulk 10 pack multi colored for students
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        // edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b> 300 €</b>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="col-3"></div>
+            {/* select menu */}
+            <div className="col-9">
+              <div className="d-flex justify-content-between align-items-lg-center">
+                <div className="filter-sort-grid">
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="mb-0 d-block">Sort By:</p>
+                    <select name="" className="form-control form-select" id="">
+                      <option value="manual">Featured</option>
+                      <option value="best-selling" selected="selected">
+                        Best Selling
+                      </option>
+                      <option value="title-ascending">
+                        Alphabetically, A-Z
+                      </option>
+                      <option value="title-ascending">
+                        Alphabetically, Z-A
+                      </option>
+                      <option value="title-ascending">
+                        Price, low to high
+                      </option>
+                      <option value="title-ascending">
+                        Price, high to low
+                      </option>
+                      <option value="title-ascending">Date, old to new</option>
+                      <option value="title-ascending">Date, new to old</option>
+                    </select>
+                  </div>
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="totalproducts">21 products</p>
+                    <div className="d-flex gap-10 align-items-center">
+                        
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
