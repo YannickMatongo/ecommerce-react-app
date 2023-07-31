f91d1f93fd7b149eed08254e9f62eb1adf2132bf
Yannick Matongo <nay77127@gmail.com>
2023-07-31 23:27:51 +0200

    creation of view fo product

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -3,8 +3,11 @@ import BreadCrumb from "../components/BreadCrumb";
 import { Helmet } from "react-helmet";
 import Meta from "../components/Meta";
 import ReactStars from "react-rating-stars-component";
+import { useState } from "react";
+import ProductCard from "../components/ProductCard";
 
-function OurStore() {
+const OurStore = () => {
+  const [grid, setGrid] = useState(4);
   return (
     <>
       <Meta title={"Our Store"} />
@@ -191,10 +194,12 @@ function OurStore() {
             </div>
             {/* select menu */}
             <div className="col-9">
-              <div className="d-flex justify-content-between align-items-lg-center">
-                <div className="filter-sort-grid">
+              <div className="filter-sort-grid mb-4">
+                <div className="d-flex justify-content-between align-items-lg-center">
                   <div className="d-flex align-items-center gap-10">
-                    <p className="mb-0 d-block">Sort By:</p>
+                    <p className="mb-0 d-block" style={{ width: "100px" }}>
+                      Sort By:
+                    </p>
                     <select name="" className="form-control form-select" id="">
                       <option value="manual">Featured</option>
                       <option value="best-selling" selected="selected">
@@ -217,11 +222,47 @@ function OurStore() {
                     </select>
                   </div>
                   <div className="d-flex align-items-center gap-10">
-                    <p className="totalproducts">21 products</p>
-                    <div className="d-flex gap-10 align-items-center">
-                        
+                    <p className="totalproducts mb-0">21 products</p>
+                    <div className="d-flex gap-10 align-items-center grid">
+                      <img
+                        onClick={() => {
+                          setGrid(3);
+                        }}
+                        src="images/gr4.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(4);
+                        }}
+                        src="images/gr3.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(6);
+                        }}
+                        src="images/gr2.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(12);
+                        }}
+                        src="images/gr.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
                     </div>
                   </div>
+                </div>
+              </div>
+              <div className="products-list pb-5">
+                <div className="d-flex gap-10 flex-wrap">
+                <ProductCard grid={grid} />
                 </div>
               </div>
             </div>
@@ -230,6 +271,6 @@ function OurStore() {
       </div>
     </>
   );
-}
+};
 
 export default OurStore;
